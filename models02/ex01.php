0bdfe2d86e528922e2d60cff81ae87621191bd0c
Truong <xuantruong0302001@gmail.com>
2021-10-27 19:18:32 +0700

    add git first

---
+++ models02/ex01.php
@@ -0,0 +1,10 @@
+<?php
+$customer = "Xuantruong";
+$customerDeltail = "xauntryong03022001";
+$customerAge = "21";
+
+echo $customer;
+echo '<br/>';
+echo $customerDeltail;
+echo '<br/>';
+echo $customerAge;
